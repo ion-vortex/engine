b1c61291736da5cc90fe8e065388549a888c5305
Ray <ray@oat.im>
2025-07-16 16:53:52 -0500

    testing(NewTests): Add basic tests for core functionality
    The additional tests cover some more basic functionality of the
    static and dynamic buffers.
    
    Changes:
     - Added more tests to `libs/core/tests/basic-test/test.cpp`

--- libs/core/tests/basic-test/test.cpp
+++ libs/core/tests/basic-test/test.cpp
@@ -4,6 +4,7 @@
 #include <memory>
 
 TEST_CASE("VectorBuffer basic test", "[vector_buffer]") {
+    using namespace std::literals::string_view_literals;
     using namespace oxide::core;
 
     SECTION("Create buffer") {
@@ -23,11 +24,82 @@ TEST_CASE("VectorBuffer basic test", "[vector_buffer]") {
     }
 
     SECTION("Append data") {
-        using namespace std::literals::string_view_literals;
         auto buffer = create_buffer(1024);
         REQUIRE(buffer.has_value());
         auto & buf = *buffer;
+        REQUIRE(buf->capacity() >= 1024);
+        REQUIRE(buf->size() == 0);
         REQUIRE(buf->append(std::as_bytes(std::span{"Hello, World!"sv})).has_value());
         REQUIRE(buf->size() == 13);
+        REQUIRE(buf->view().size() == 13);
+        REQUIRE(std::string_view(reinterpret_cast<const char*>(buf->view().data()), buf->view().size()) == "Hello, World!");
+    }
+
+    SECTION("Resize buffer") {
+        auto buffer = create_buffer(1024);
+        REQUIRE(buffer.has_value());
+        auto & buf = *buffer;
+        REQUIRE(buf->resize(512).has_value());
+        REQUIRE(buf->size() == 512);
+        REQUIRE(buf->capacity() >= 512);
+    }
+
+    SECTION("Clear buffer") {
+        auto buffer = create_buffer(1024);
+        REQUIRE(buffer.has_value());
+        auto & buf = *buffer;
+        REQUIRE(buf->append(std::as_bytes(std::span{"Test data"sv})).has_value());
+        REQUIRE(buf->size() > 0);
+        REQUIRE(buf->clear().has_value());
+        REQUIRE(buf->size() == 0);
+    }
+
+    SECTION("Shrink to fit") {
+        auto buffer = create_buffer(1024);
+        REQUIRE(buffer.has_value());
+        auto & buf = *buffer;
+        REQUIRE(buf->append(std::as_bytes(std::span{"Data to shrink"sv})).has_value());
+        REQUIRE(buf->size() > 0);
+        REQUIRE(buf->shrinkToFit().has_value());
+        REQUIRE(buf->capacity() >= buf->size());
+    }    
+}
+
+TEST_CASE("StaticBuffer basic test", "[static_buffer]") {
+    using namespace std::literals::string_view_literals;
+    using namespace oxide::core;
+
+    SECTION("Create static buffer") {
+        auto buffer = create_static_buffer<256>();
+        REQUIRE(buffer.has_value());
+        auto & buf = *buffer;
+        REQUIRE(buf->size() == 0);
+        REQUIRE(buf->capacity() == 256);
+    }
+
+    SECTION("Append to static buffer") {
+        auto buffer = create_static_buffer<256>();
+        REQUIRE(buffer.has_value());
+        auto & buf = *buffer;
+        REQUIRE(buf->append(std::as_bytes(std::span{"Static data"sv})).has_value());
+        REQUIRE(buf->size() == 11);
+    }
+
+    SECTION("Resize static buffer") {
+        auto buffer = create_static_buffer<256>();
+        REQUIRE(buffer.has_value());
+        auto & buf = *buffer;
+        REQUIRE(buf->resize(128).has_value());
+        REQUIRE(buf->size() == 128);
+    }
+
+    SECTION("Clear static buffer") {
+        auto buffer = create_static_buffer<256>();
+        REQUIRE(buffer.has_value());
+        auto & buf = *buffer;
+        REQUIRE(buf->append(std::as_bytes(std::span{"Clear me"sv})).has_value());
+        REQUIRE(buf->size() > 0);
+        REQUIRE(buf->clear().has_value());
+        REQUIRE(buf->size() == 0);
     }
 }
